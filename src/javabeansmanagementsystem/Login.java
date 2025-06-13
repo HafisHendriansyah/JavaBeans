a36aa2c80aee77f1df71b2c550424dc370cbf44a
HAFIS HENDRIANSYAH <HAFIS HENDRIANSYAH@LAPTOP-8BN42G8D>
2025-06-13 18:17:49 +0700

    first commit

---
+++ src/javabeansmanagementsystem/Login.java
@@ -0,0 +1,15 @@
+package javabeansmanagementsystem;
+
+/**
+ *
+ * @author HAFIS HENDRIANSYAH
+ */
+public class Login {
+    public String username;
+    public String password;
+    
+    public Login(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/javabeansmanagementsystem/SignUp.java
@@ -0,0 +1,19 @@
+package javabeansmanagementsystem;
+
+/**
+ *
+ * @author HAFIS HENDRIANSYAH
+ */
+public class SignUp {
+    public String nameUser;
+    public String username;
+    public String email;
+    public String passwordUser;
+    
+    public SignUp(String nameUser, String username, String email, String passwordUser){
+        this.nameUser = nameUser;
+        this.username = username;
+        this.email = email;
+        this.passwordUser = passwordUser;
+    }
+}
